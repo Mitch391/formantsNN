7e18555214f65bdd5edc5d279a496259b5548ab0
Michel van Hoof <mitch391@gmail.com>
2024-03-14 23:35:32 +0100

    rename

--- sound_to_spectrogram.py
+++ sound_to_spectrogram.py
@@ -26,7 +26,7 @@ def convert_spectrogram_png(spectrogram, start=189, end=189+settings.spectrogram
     
 def create_spectrogram_png_and_txt_script(source, dest_png, dest_txt):
     script = "Read from file: \"" + source + "\"\n"
-    script += "To Spectrogram: 0.005, 5000, 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
+    script += "To Spectrogram: 0.005, " + settings.spectrogram_max_Hz + ", 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
     script += "Save as 300-dpi PNG file: \"" + dest_png + "\"\n"
     script += "To Matrix\n"
     script += "Save as headerless spreadsheet file: \"" + dest_txt + "\""
@@ -36,7 +36,7 @@ def create_spectrogram_png_and_txt_script(source, dest_png, dest_txt):
 
 def create_spectrogram_png_script(source, dest):
     script = "Read from file: \"" + source + "\"\n"
-    script += "To Spectrogram: 0.005, 5000, 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
+    script += "To Spectrogram: 0.005, " + settings.spectrogram_max_Hz + ", 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
     script += "Save as 300-dpi PNG file: \"" + dest + "\""
     f = open(settings.cwd + "Create_spectrogram_from_sound.praat", "w")
     f.write(script)
@@ -45,7 +45,7 @@ def create_spectrogram_png_script(source, dest):
     
 def create_spectrogram_txt_script(source, dest):
     script = "Read from file: \"" + source + "\"\n"
-    script += "To Spectrogram: 0.005, 5000, 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
+    script += "To Spectrogram: 0.005, " + settings.spectrogram_max_Hz + ", 0.002, 20, \"Gaussian\"\nPaint: 0, 0, 0, 0, 100, \"yes\", 50, 6, 0, \"no\"\n"
     script += "To Matrix\n"
     script += "Save as headerless spreadsheet file: \"" + dest + "\""
     f = open(settings.cwd + "Create_spectrogram_txt.praat", "w")
@@ -66,22 +66,26 @@ def spectrogram_png_to_data():
     
 def spectrogram_txt_to_data():
     list_of_sounds = []
-    for sound in os.listdir(settings.spectrograms_text_dest):
-        spectrogram = np.loadtxt(settings.spectrograms_text_dest + sound[:-4] + '.txt', delimiter='\t')
+    for sound in os.listdir(settings.spectrograms_txt_dest):
+        spectrogram = np.loadtxt(settings.spectrograms_txt_dest + sound[:-4] + '.txt', delimiter='\t')
         spectrogram = convert_spectrogram_txt(spectrogram)
         list_of_sounds.append([int(sound[:-4]), spectrogram])
     return list_of_sounds
 
 
 def generate_spectrograms():
-    if os.path.isdir("./data/spectrograms"):
-        shutil.rmtree("./data/spectrograms")
-    os.mkdir("./data/spectrograms")
-    os.mkdir("./data/spectrograms/pngs")
-    os.mkdir("./data/spectrograms/txts")
+    if os.path.isdir(settings.spectrograms_dest):
+        shutil.rmtree(settings.spectrograms_dest)
+    os.mkdir(settings.spectrograms_dest)
+    os.mkdir(settings.spectrograms_png_dest)
+    os.mkdir(settings.spectrograms_txt_dest)
     for sound in os.listdir(settings.sound_files_src):
         source = settings.sound_files_src + sound
         dest_png = settings.spectrograms_png_dest + sound[:-4] + ".png"
-        dest_txt = settings.spectrograms_text_dest + sound[:-4] + ".txt"
+        dest_txt = settings.spectrograms_txt_dest + sound[:-4] + ".txt"
         create_spectrogram_png_and_txt_script(source, dest_png, dest_txt)
         subprocess.call([settings.praat_src, '--run', 'Create_spectrogram_from_sound.praat'])
+
+
+if __name__ == '__main__':
+    generate_spectrograms()
